1519a9680ddfa3fad2b10d23e30a0f290e13cbed
MagicSqe <MagicSqe0606@petalmail.com>
2022-06-09 17:39:43 +0800

    elementplus

---
+++ src/views/login/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>{{ info }}</div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue"
+const info = ref("hello world")
+</script>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
